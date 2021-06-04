5dff8151108742b43ccb9c1646fa26ddff256f01
sylviachach <sylviachach@gmail.com>
2021-06-04 15:00:11 +0300

    Added Inheritance

--- weather-api/src/main/java/com/example/weatherapi/WeatherApiApplication.java
+++ weather-api/src/main/java/com/example/weatherapi/WeatherApiApplication.java
@@ -1,10 +1,12 @@
 package com.example.weatherapi;
 
+import com.example.weatherapi.prediction.Data;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherApiApplication {
+public class WeatherApiApplication extends Data {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiApplication.class, args);
